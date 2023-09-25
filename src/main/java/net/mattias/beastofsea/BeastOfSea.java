8047087b5cb5748854915ec99204cecc8b92db0b
Mattias150784 <Mattiasayenew@gmail.com>
2023-09-24 21:11:04 -0700

    Initial commit

---
+++ src/main/java/net/mattias/beastofsea/BeastOfSea.java
@@ -0,0 +1,34 @@
+package net.mattias.beastofsea;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+@Mod(BeastOfSea.MOD_ID)
+public class BeastOfSea {
+    public static final String MOD_ID = "beastofsea";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public BeastOfSea()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
+
